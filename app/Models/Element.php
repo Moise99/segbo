b52de8c84a9ada64a7e73befd15b00691549bf25
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-09-22 16:47:17 +0000

    add element model

--- app/Models/Element.php
+++ app/Models/Element.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Element extends Model
 {
-    //
+    /** @use HasFactory */
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'link',
+        'cover',
+        'desc',
+        'user_id',
+        'elementype_id',
+        'categorie_id',
+    ];
+
+    protected function casts() : array
+    {
+        return[
+            'id' => 'integer',
+            'title' => 'string',
+            'link' => 'string',
+            'desc' => 'string',
+            'cover' => 'string',
+            'user_id' => 'integer',
+            'elementype_id' => 'integer',
+            'categorie_id' => 'integer',
+        ];
+    }
 }
