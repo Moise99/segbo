8e5442d3caee97f37a3680c20526211bd81aef69
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-09-22 16:51:04 +0000

    add element controller function new

--- app/Http/Controllers/ElementController.php
+++ app/Http/Controllers/ElementController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Bdepense;
 
 class ElementController extends Controller
 {
-    //
+    public function new() {
+        return Inertia::render('element.new');
+    }
+
+    public function list() {
+        return Inertia::render('element.list');
+    }
+
+    public function edit() {
+        return Inertia::render('element.edit');
+    }
 }
