aa4ad8e57816e5222074bf117bfce3cad716a611
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-09-23 11:32:04 +0000

    acdetail migration and model done

---
+++ app/Models/Acdetail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Acdetail extends Model
+{
+    protected $fillable = [
+        'photo',
+        'present',
+        'linkedin',
+        'facebook',
+        'instagram',
+        'x',
+    ];
+
+    protected function casts() : array
+    {
+        return[
+            'id' => 'integer',
+            'photo' => 'string',
+            'present' => 'string',
+            'linkedin' => 'string',
+            'facebook' => 'string',
+            'user_id' => 'integer',
+            'instagram' => 'string',
+            'x' => 'string',
+        ];
+    }
+}
