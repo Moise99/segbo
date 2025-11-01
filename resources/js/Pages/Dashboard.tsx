3c9bda08ea9964e6b6cf2c78bbb9b55ecd380a13
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-11-01 21:09:05 +0000

    message to dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -10,7 +10,8 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            Wecome to Segbon! Be part of Something great
+                            Wecome to Segbon! Be part of Something great You are
+                            logged in!
                         </div>
                     </div>
                 </div>
